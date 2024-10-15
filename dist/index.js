4a54c431d1e80612a32157f2bf7165c57d64cd19
weberankit <codingank@gmail.com>
2024-10-15 21:55:36 +0530

    ui-setup done

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("jj");
+let row = 100;
+let colos = 26;
+let addressColCont = document.querySelector(".address-col-cont");
+console.log(addressColCont);
+for (let i = 0; i < row; i++) {
+    let addressCol = document.createElement("div");
+    // ts--innertext expect string
+    addressCol.innerText = (i + 1).toString();
+    addressColCont === null || addressColCont === void 0 ? void 0 : addressColCont.appendChild(addressCol);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("jj")
+let row=100
+let colos=26
+
+
+let addressColCont=document.querySelector(".address-col-cont") as HTMLElement
+console.log(addressColCont)
+for(let i=0;i<row;i++){
+    let addressCol=document.createElement("div") as HTMLElement
+    // ts--innertext expect string
+    addressCol.innerText=(i+1).toString()
+    addressColCont?.appendChild(addressCol)
+}
