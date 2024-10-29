02467a4b5d0dd9518b544bfa4c9068e7bb322b21
weberankit <codingank@gmail.com>
2024-10-29 14:38:35 +0530

    ui part done

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,47 @@
 let row = 100;
 let colos = 26;
 let addressColCont = document.querySelector(".address-col-cont");
-console.log(addressColCont);
+let addressRowCont = document.querySelector(".address-row-cont");
+let cellCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
+console.log(addressBar);
+// for col- 1,2,3.....
 for (let i = 0; i < row; i++) {
     let addressCol = document.createElement("div");
     // ts--innertext expect string
+    addressCol.setAttribute("class", "address-col");
     addressCol.innerText = (i + 1).toString();
     addressColCont === null || addressColCont === void 0 ? void 0 : addressColCont.appendChild(addressCol);
+}
+// for A,B,C,D--row wise
+for (let i = 0; i < colos; i++) {
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    //fromCharCode A,B,C see mdn
+    addressRow.innerText = String.fromCharCode(65 + i);
+    addressRowCont === null || addressRowCont === void 0 ? void 0 : addressRowCont.appendChild(addressRow);
+}
+//creating cell 
+for (let i = 0; i < row; i++) {
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "row-cont");
+    for (let j = 0; j < colos; j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        rowCont.appendChild(cell);
+        //using to find cell address when user click on cell
+        addEventListnerForAddressBar(cell, i, j);
+    }
+    cellCont === null || cellCont === void 0 ? void 0 : cellCont.appendChild(rowCont);
+}
+//to find cell address
+function addEventListnerForAddressBar(cell, i, j) {
+    cell.addEventListener("click", () => {
+        const colId = String.fromCharCode(i + 65);
+        //as j start from 0 so 0+1
+        const rowId = j + 1;
+        console.log(rowId);
+        addressBar.value = `${colId}${rowId}`;
+    });
 }
